25775428235a69824d882d4fb8e7f9eebe35ac93
Franco Lombardo <f.lombardo69@gmail.com>
2020-12-09 10:49:21 +0100

    Moving to Kotlin 1.3 - Added map method - Added more tests

--- src/test/kotlin/com/codepoetics/klenses/LensTest.kt
+++ src/test/kotlin/com/codepoetics/klenses/LensTest.kt
@@ -5,7 +5,26 @@ import org.junit.Test
 
 class LensTest {
 
-    @Test fun lensesCompose(): Unit {
+    @Test
+    fun basicExample() {
+        data class Inner(val value: String)
+        data class Outer(val outerValue: String, val inner: Inner?)
+
+        val foo = Outer("foo", null)
+
+        val outerValueLens = +Outer::outerValue
+        val innerLens = Outer::inner orElse Inner("xyzzy")
+        val innerValueLens = innerLens + Inner::value
+
+        assertEquals("foo", outerValueLens(foo))
+        assertEquals(Outer("quux", null), outerValueLens(foo, "quux"))
+        assertEquals(Inner("xyzzy"), innerLens(foo))
+        assertEquals(Outer("foo", Inner("frobnitz")), innerValueLens(foo, "frobnitz"))
+        assertEquals(Outer("foo", Inner("XYZZY")), innerValueLens(foo) { toUpperCase() })
+    }
+
+    @Test
+    fun lensesCompose(): Unit {
         data class Inner(val value: String)
         data class Outer(val outerValue: String, val inner: Inner?)
 
@@ -20,7 +39,8 @@ class LensTest {
         assertEquals(Outer("foo", Inner("frobnitz")), innerValueLens(foo, "frobnitz"))
     }
 
-    @Test fun extraProperties(): Unit {
+    @Test
+    fun extraProperties(): Unit {
         data class Foo(val value: String) {
             val uppercased = value.toUpperCase()
         }
@@ -32,11 +52,12 @@ class LensTest {
             val illegal = +Foo::uppercased
             fail("Illegal lens creation should throw an exception")
         } catch (e: IllegalArgumentException) {
-             assertTrue(e.message!!.contains("not used in constructor"))
+            assertTrue(e.message!!.contains("not used in constructor"))
         }
     }
 
-    @Test fun implicitConversion(): Unit {
+    @Test
+    fun implicitConversion(): Unit {
         data class Inner(val value: String)
         data class Outer(val outerValue: String, val inner: Inner)
 
@@ -51,7 +72,8 @@ class LensTest {
         assertEquals(Outer("baz", Inner("bar")), Outer::outerValue.set(foo, "baz"))
     }
 
-    @Test fun updating(): Unit {
+    @Test
+    fun updating(): Unit {
         data class Inner(val value: Int)
         data class Outer(val outerValue: String, val inner: Inner)
 
@@ -61,4 +83,44 @@ class LensTest {
 
         assertEquals(Outer("foo", Inner(46)), innerValueLens(foo) { times(2) })
     }
+
+    @Test
+    fun propertyMapper(): Unit {
+        data class Inner(val value: Int)
+        data class Outer(val outerValue: String, val inner: Inner)
+
+        val foo = Outer("foo", Inner(23))
+
+        val mapper = PropertyMapper.forKClass(Outer::class)
+
+        val outerSetter = mapper.setterFor(Outer::outerValue)
+
+        assertEquals(
+            Outer("newFoo", Inner(23)),
+            outerSetter(foo, "newFoo")
+        )
+
+        assertEquals(
+            Outer("anotherFoo", Inner(23)),
+            mapper.copy(foo, Outer::outerValue, "anotherFoo")
+        )
+    }
+
+    @Test
+    fun mapping() {
+        data class Inner(val value: Int)
+        data class Outer(val outerValue: String, val inners: List<Inner>)
+
+        val foo = Outer("foo", listOf(Inner(1), Inner(2)))
+
+        val innerLens = Outer::inners.lens()
+        val innerValueLens = Inner::value.lens()
+
+        val newOuter = innerLens.map(foo) {
+            innerValueLens(this) { times(2) }
+        }
+
+        assertEquals(Outer("foo", listOf(Inner(2), Inner(4))), newOuter)
+    }
+
 }
